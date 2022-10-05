c15fd089cceae56502ec93c2382eb5495d9f4d96
Soumindar Qolby <soumindarqolby@gmail.com>
2022-10-05 13:38:50 +0700

    update nestedFor

--- Pekan1/nestedFor.js
+++ Pekan1/nestedFor.js
@@ -1,21 +1,26 @@
-let pola = "";
-
 for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
         if (i == 0 || i == 4) {
-            pola += "*";
+            document.write("*");
         } else if (j == 0 || j == 4) {
-            pola += "*";
+            document.write("*");
         } else {
-            pola += " ";
+            document.write(" ");
         }
     }
-    pola += "\n";
+    document.write("</br>");
 }
 
-console.log(pola);
+// console.log(pola);
 
 // output tanpa newline
 // process.stdout.write("tes123");
 // process.stdout.write("tes345");
+
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 0; j < 5; j++) {
+//         document.write("*");
+//     }
+//     document.write("</br>");
+// }
 
