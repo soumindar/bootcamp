fa379e446cdcf80ec5744e2eabda908d78db01c3
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-06 10:55:33 +0700

    Update nestedFor.js

--- Pekan1/nestedFor.js
+++ Pekan1/nestedFor.js
@@ -1,20 +1,23 @@
-for (let i = 0; i < 5; i++) {
-    for (let j = 0; j <= i; j++) {
-        document.write("*");
-    }
-    document.write("</br>");
-}
-
-// console.log(pola);
-
-// output tanpa newline
-// process.stdout.write("tes123");
-// process.stdout.write("tes345");
-
 // for (let i = 0; i < 5; i++) {
-//     for (let j = 0; j < 5; j++) {
+//     for (let j = 0; j <= i; j++) {
 //         document.write("*");
 //     }
 //     document.write("</br>");
 // }
 
+let n = 5;
+let countLine = 0;
+let countStar = 0
+for (let i = 1; i <= (1+n)*n/2; i++) {
+    process.stdout.write("*");
+    countStar++;
+    if (countStar > countLine) {
+        process.stdout.write("\n");
+        countLine++;
+        countStar = 0;
+    }
+}
+
+// output tanpa newline
+// process.stdout.write("tes123");
+// process.stdout.write("tes345");
