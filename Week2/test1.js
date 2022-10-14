9edccfda0e6fba96f59f198e7bcecb4c4e2d8d3a
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-14 10:44:52 +0700

    Update test1.js

--- Week2/test1.js
+++ Week2/test1.js
@@ -1,18 +1,3 @@
 let items = ['buku', 'pensil', 'penggaris'];
-let obj1 = {
-    key1: 'value1',
-    key2: 'value2'
-}
-
-let obj2 = {
-    key21: 'value21',
-    key22: 'value22'
-}
-
-// let newObj = {...obj1, ...obj2};
-let newArr = {...items, ...items};
+let newArr = items.map(item => `${item}!`);
 console.log(newArr);
-
-for (let i in arr) {
-    asd
-}
