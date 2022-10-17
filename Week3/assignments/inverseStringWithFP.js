efb1c754b13534e1e711a06176d8cabe63cc0782
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-17 14:52:53 +0700

    Update inverseStringWithFP.js

--- Week3/assignments/inverseStringWithFP.js
+++ Week3/assignments/inverseStringWithFP.js
@@ -1,9 +1,34 @@
 let strings = ['qwe', 'asd', 'zxc'];
 
-const reversingStr = (i, str) => (i >= 0) ? (str[i] + reversingStr(--i, str)) : '';
+const reversingStr = str => {
+    let revStr = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        revStr += str[i];
+    }
+
+    return revStr;
+}
+
+const reversingStrRecursive = (i, str) => (i >= 0) ? (str[i] + reversingStrRecursive(--i, str)) : '';
+
+const reversingStrRecExplained = function (i, str) {
+    if (i < 0) {
+        return '';
+    } else {
+        return str[i] + reversingStrRecExplained(--i, str);
+    }
+}
 
 let reversedStrings = strings.map(str => reversingStr(str));
-let reversedStrings2 = strings.map(str => [...str].reverse().join(''));
+let reversedStringsFP = strings.map(str => [...str].reverse().join(''));
+let reversedStringsRecursive = strings.map(str => reversingStrRecursive(str.length-1, str));
 
 console.log(reversedStrings);
-console.log(reversedStrings2);
+console.log(reversedStringsFP);
+console.log(reversedStringsRecursive);
+
+// 'qwe'
+// (2, 'qwe') = e + (1, 'qwe') = 'e' + 'wq' = 'ewq'
+// (1, 'qwe') = w + (0, 'qwe') = 'w' + 'q' = 'wq'
+// (0, 'qwe') = q + (-1, 'qwe') = 'q' + '' = 'q'
+// (-1, 'qwe') = ''
