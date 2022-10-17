6fed2c70a0dd28b52a7ee5c23969fd1213465999
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-17 14:33:53 +0700

    Update inverseStringWithFP.js

--- Week3/assignments/inverseStringWithFP.js
+++ Week3/assignments/inverseStringWithFP.js
@@ -1,13 +1,6 @@
 let strings = ['qwe', 'asd', 'zxc'];
 
-const reversingStr = str => {
-    let revStr = ''
-    for (let i = (str.length-1); i >= 0; i--) {
-        revStr += str[i];
-    }
-
-    return revStr;
-}
+const reversingStr = (i, str) => (i >= 0) ? (str[i] + reversingStr(--i, str)) : '';
 
 let reversedStrings = strings.map(str => reversingStr(str));
 let reversedStrings2 = strings.map(str => [...str].reverse().join(''));
