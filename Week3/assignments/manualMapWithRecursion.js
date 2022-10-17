1f88b819efc98cebb120ada3ce3c8515aab72a5a
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-17 15:55:30 +0700

    Update manualMapWithRecursion.js

--- Week3/assignments/manualMapWithRecursion.js
+++ Week3/assignments/manualMapWithRecursion.js
@@ -3,18 +3,18 @@ let data = [1, 2, 3];
 const makeArrayWithMap = (data, f) => {
     let arr = [];
 
-    const map = (data, i, f) => {
-        if (i == data.length-1) {
-            arr.push(f(data[i]));
+    const map = (data, f, i = 0) => {
+        if (i > data.length-1) {
+            return 'success';
         } else {
             arr.push(f(data[i]))
-            map(data, ++i, f); 
+            return map(data, f, ++i);
         }
     }
 
-    map(data, 0, f);
+    map(data, f);
     return arr;
-} 
+}
 
 let dataPlusOne = makeArrayWithMap(data, x => x+1);
 console.log(dataPlusOne);
