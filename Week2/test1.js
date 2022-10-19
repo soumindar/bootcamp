a6ba5f2d14cf1dc177bc98180e5babca4556bfcf
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-19 13:31:46 +0700

    Update test1.js

--- Week2/test1.js
+++ Week2/test1.js
@@ -1,11 +1,1 @@
-const createObj = (person, age) => ({...person, age});
-
-const person = {
-    name: 'sou',
-    age: 24,
-    city: 'pwt'
-}
-
-let newPerson = {...person, age: 22};
-
-console.log(person, newPerson);
+console.log(Number('0'));
