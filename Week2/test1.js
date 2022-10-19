22261ab035c782d90365b92a030e5d5d1a3eab37
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-19 14:17:10 +0700

    Update test1.js

--- Week2/test1.js
+++ Week2/test1.js
@@ -1,1 +1,12 @@
-console.log(Number('0'));
+try {
+    // throw 'asd';
+    try {
+        throw 'qwe';
+    } catch (e) {
+        console.log(e);
+    } finally {
+        console.log('asd');
+    }
+} catch (e) {
+    console.log(e);
+}
