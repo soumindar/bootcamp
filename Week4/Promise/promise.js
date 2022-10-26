a8db06510088212f179e8ca9bd5040b6d3514745
Sou <33419855+soumindar@users.noreply.github.com>
2022-10-26 11:39:11 +0700

    Update promise.js

--- Week4/Promise/promise.js
+++ Week4/Promise/promise.js
@@ -13,10 +13,16 @@ const printThis = str => {
     console.log(str);
 };
 
-// promise1.then(printThis);
+console.log('mulai');
+promise1
+    .finally(() => {console.log('finally1')})
+    .then(printThis);
+console.log('selesai');
+
 
 const promise2 = new Promise((resolve) => {
     if (a) {
+        console.log('test2');
         setTimeout(() => {
             resolve('ok2');
         }, 1000);
@@ -27,23 +33,23 @@ const promise2 = new Promise((resolve) => {
     }
 });
 
-console.log('mulai');
-console.log(promise2);
+// console.log('mulai');
+// console.log(promise2);
 
-promise2
-    .finally(() => {console.log(promise2)})
-    .then(str => {
-        console.log(str);
-        setTimeout(() => {
-            return new Promise((resolve) => {
-                resolve('ok3');
-            });
-        }, 2000);
-    })
-    .then(str => {
-        setTimeout(() => {
-            console.log(str);
-        }, 1000);
-    });
+// promise2
+//     .finally(() => {console.log(promise2)})
+//     .then(str => {
+//         console.log(str);
+//         setTimeout(() => {
+//             return new Promise((resolve) => {
+//                 resolve('ok3');
+//             });
+//         }, 2000);
+//     })
+//     .then(str => {
+//         setTimeout(() => {
+//             console.log(str);
+//         }, 1000);
+//     });
 
-console.log('selesai');
+// console.log('selesai');
